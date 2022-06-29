07a929456c852e6d02221b90f1dc1f166fd67031
Adnan Khan <adnan7.zain@gmail.com>
2022-06-29 13:04:59 +0500

    Dark Mode - moment.js

--- src/Article.js
+++ src/Article.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import moment from 'moment';
 
 export const Article = ({ title, date, snippet, length }) => {
   return (
     <article className='post'>
       <h2>{title}</h2>
       <div className='post-info'>
-        <span>date</span>
-        <span>{length} min read</span>
+        <span>{moment(date).format('MMMM ddd Do YYYY')}</span>
+        <span>{length} min read </span>
       </div>
       <p>{snippet}</p>
     </article>
